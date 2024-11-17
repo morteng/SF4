bb1f489ebd58968bfd95753f0b7e07729e58db8d
Morten <morteng@gmail.com>
2024-11-17 21:21:09 +0100

    refactor: Remove unused bot and user route files

--- app/routes/bot_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-bot_bp = Blueprint('bot', __name__, url_prefix='/bot')
-
-@bot_bp.route('/status')
-def bot_status():
-    return jsonify({"status": "All bots operational"}), 200
