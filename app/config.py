883f5a4543167fd44f6d908ef9c88dbe6989a223
Morten <morteng@gmail.com>
2024-11-21 12:11:36 +0100

    feat: Add database configuration to config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,10 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_very_secret_key'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(BASE_DIR, '..', 'instance', 'site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DefaultConfig(Config):
