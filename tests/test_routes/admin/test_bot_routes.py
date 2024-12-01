813fb46767bbd7ee764df7a8294adb6b91a33d4c
Morten <morteng@gmail.com>
2024-12-01 16:16:51 +0100

    test: Remove unused test files

--- tests/test_routes/admin/test_bot_routes.py
+++
@@ -1,1 +0,0 @@
-# This file has been removed as the bot creation API is not being implemented at this time.
--- tests/test_routes/general_bot_routes.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-def test_bot_status(client):
-    response = client.get('/bot/status')
-    assert response.status_code == 200
-    assert b'"status": "All bots operational"' in response.data
