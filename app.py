8d8cbc992b825b19254cd553c6b8e715913e6cc7
Morten <morteng@gmail.com>
2024-12-05 09:14:13 +0100

    refactor: simplify app initialization and template loading

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
+import os 
 from flask import Flask, flash, redirect, url_for, render_template
 from flask_login import LoginManager  # Import LoginManager
 from app.routes.visitor_routes import visitor_bp  # Import the blueprint
 from app.models.user import User  # Import the User model
+from app import create_app
 
-app = Flask(__name__)
+template_dir = os.path.abspath('app/templates')
+app = Flask(__name__, template_folder=template_dir)
 app.secret_key = 'your_secret_key'
 
-# Initialize Flask-Login
-login_manager = LoginManager()
-login_manager.init_app(app)
 
-# Register the blueprint
-app.register_blueprint(visitor_bp)
 
 @app.route('/set_flash')
 def set_flash():
@@ -22,10 +20,9 @@ def set_flash():
 def index():
     return render_template('index.html')
 
-# User loader callback function
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+
 
 if __name__ == '__main__':
+    app = create_app()
+    print(f"Serving templates from: {app.template_folder}")
     app.run(debug=True)
