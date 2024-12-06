600a19b219872eeea0ec9a1042f49c22af7cd134
Morten <morteng@gmail.com>
2024-12-06 11:54:12 +0100

    refactor: Remove unused API routes and update default secret key

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key_here')
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///instance/site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
