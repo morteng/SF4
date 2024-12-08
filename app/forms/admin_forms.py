28b264d2aa79d498f51edbd8af84ba2a6241add2
Morten Gulden <morteng@gmail.com>
2024-12-08 18:04:38 +0100

    fix: Increase organization name length and update form size

--- app/forms/admin_forms.py
+++ app/forms/admin_forms.py
@@ -5,7 +5,7 @@ from app.models.stipend import Stipend
 from datetime import datetime
 
 class OrganizationForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=100)])
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=255)])
     description = TextAreaField('Description', validators=[Length(max=255)])
     homepage_url = StringField('Homepage URL', validators=[Length(max=255)])
     submit = SubmitField('Create')
