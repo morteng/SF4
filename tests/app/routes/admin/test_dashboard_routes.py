484de468a4818383b4d9d36db5f3cd5fba74bb17
Morten Gulden <morteng@gmail.com>
2024-12-08 12:49:40 +0100

    fix: Correct URL for admin dashboard data route

--- tests/app/routes/admin/test_dashboard_routes.py
+++ tests/app/routes/admin/test_dashboard_routes.py
@@ -2,7 +2,7 @@ from flask import url_for
 import pytest
 
 def test_dashboard_data(client, app):
-    response = client.get(url_for('admin_dashboard.data'))
+    response = client.get(url_for('admin.admin_dashboard.data'))
     assert response.status_code == 200
     assert b'Stipend Count:' in response.data
     assert b'Recent Bot Logs:' in response.data
