e7f6d7edb3c6ac541aa1da1a111d485b028e8255
Morten Gulden <morteng@gmail.com>
2024-12-08 12:54:11 +0100

    fix: Update admin route URLs and test cases

--- tests/app/routes/admin/test_dashboard_routes.py
+++ tests/app/routes/admin/test_dashboard_routes.py
@@ -3,7 +3,7 @@ import pytest
 
 def test_dashboard_data(client, app):
     with app.test_request_context():
-        response = client.get(url_for('admin.dashboard'))
+        response = client.get(url_for('admin.admin_dashboard.data'))
         assert response.status_code == 200
         assert b'Stipend Count:' in response.data
         assert b'Recent Bot Logs:' in response.data
