73774af688956e3d08a014c892093cba54379758
Morten Gulden <morteng@gmail.com>
2024-12-29 13:51:59 +0100

    chore: Remove unused files and dependencies

--- conftest.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import pytest
-
-def pytest_sessionstart(session):
-    """Run dependency verification before test session starts"""
-    try:
-        subprocess.check_call(["pip", "install", "-r", "requirements.txt"])
-    except subprocess.CalledProcessError:
-        pytest.exit("Failed to install dependencies - aborting test session")
--- forms/custom_fields.py
+++
@@ -1,8 +0,0 @@
-from wtforms import Field
-from wtforms.validators import InputRequired
-
-class CustomDateTimeField(Field):
-    def __init__(self, label=None, validators=None, **kwargs):
-        if validators is None:
-            validators = [InputRequired()]  # Default validator
-        super().__init__(label=label, validators=validators, **kwargs)
